835fbe4fb1ca072a957630f19ef693929b30783d
Michel <github@chromadigital.group>
2019-01-05 19:53:04 +0100

    updated mono install

--- misc/install-mono.sh
+++ misc/install-mono.sh
@@ -1,15 +1,12 @@
 #!./bin/bash
 
-apt-get update
-apt-get upgrade -y
-
-apt install dirmngr -y
+apt install apt-transport-https dirmngr -y
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 
-echo "deb http://download.mono-project.com/repo/debian stretch main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
-sudo apt-get update
+sudo apt update -y
 
 sudo apt-get install mono-devel mono-complete mono-dbg referenceassemblies-pcl ca-certificates-mono mono-xsp4 -y
 
