0d3ed72ade40a3f39a3667343cedbd64b5df6c14
Michel <github@chromadigital.group>
2019-01-17 21:50:26 +0100

    update

--- misc/install-teamspeak.sh
+++ misc/install-teamspeak.sh
@@ -5,9 +5,12 @@
 apt-get update
 apt-get -y upgrade
 
+# Install dependencies
+apt-get install bzip2 xz-utils -y
+
 # Add a user
 
-adduser --gecos "" teamspeak
+adduser teamspeak
 
 # Switch user
 
@@ -17,7 +20,7 @@ cd /home/teamspeak
 
 # download teamspeak
 
-wget -e --output-document=ts3.tar.bz2 -nv http://dl.4players.de/ts/releases/3.3.1/teamspeak3-server_linux_amd64-3.3.1.tar.bz2
+wget -e --output-document=ts3.tar.bz2 -nv https://files.teamspeak-services.com/releases/server/3.5.1/teamspeak3-server_linux_amd64-3.5.1.tar.bz2
 
 # unzip
 
@@ -28,3 +31,6 @@ rm ts3.tar.bz2
 # start
 
 ./ts3server_startscript.sh start
+
+# move teamspeak3 into /etc/init.d/
+# update-rc.d teamspeak3 defaults
